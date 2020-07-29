5385095f324e4dce48780fc45d0cd2cd3fe04ca6
admin@javaops.ru <gitlaB13>
2020-07-29 17:51:15 +0300

    8_07_ajax_datatables

---
+++ src/main/java/ru/javawebinar/topjava/web/user/AdminUIController.java
@@ -0,0 +1,40 @@
+package ru.javawebinar.topjava.web.user;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import ru.javawebinar.topjava.model.Role;
+import ru.javawebinar.topjava.model.User;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin/users")
+public class AdminUIController extends AbstractUserController {
+
+    @Override
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<User> getAll() {
+        return super.getAll();
+    }
+
+    @Override
+    @DeleteMapping("/{id}")
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable int id) {
+        super.delete(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void createOrUpdate(@RequestParam Integer id,
+                               @RequestParam String name,
+                               @RequestParam String email,
+                               @RequestParam String password) {
+
+        User user = new User(id, name, email, password, Role.USER);
+        if (user.isNew()) {
+            super.create(user);
+        }
+    }
+}
--- src/main/webapp/WEB-INF/jsp/fragments/headTag.jsp
+++ src/main/webapp/WEB-INF/jsp/fragments/headTag.jsp
@@ -10,5 +10,12 @@
     <link rel="stylesheet" href="resources/css/style.css">
     <link rel="stylesheet" href="webjars/bootstrap/4.5.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="webjars/noty/3.1.4/demo/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="webjars/datatables/1.10.21/css/dataTables.bootstrap4.min.css">
     <link rel="shortcut icon" href="resources/images/icon-meal.png">
+
+    <%--http://stackoverflow.com/a/24070373/548473--%>
+    <script type="text/javascript" src="webjars/jquery/3.5.1/jquery.min.js" defer></script>
+    <script type="text/javascript" src="webjars/bootstrap/4.5.0/js/bootstrap.min.js" defer></script>
+    <script type="text/javascript" src="webjars/datatables/1.10.21/js/jquery.dataTables.min.js" defer></script>
+    <script type="text/javascript" src="webjars/datatables/1.10.21/js/dataTables.bootstrap4.min.js" defer></script>
 </head>
